8839b66fbd717d1dc79c470eb00c4b6f17b71435
jepkess <biivincent1997@gmail.com>
2021-09-21 23:23:13 +0300

    modify script file

---
+++ Account/js/script.js
@@ -0,0 +1,13 @@
+var input = document.getElementById("name");
+localStorage.setItem("name", input.val());
+var input = document.getElementById("exampleInputEmail1");
+// localStorage.setItem("exampleInputEmail1", input.val());
+var input = document.getElementById("exampleInputPassword1");
+localStorage.setItem("exampleInputPassword1", input.val());
+var input = document.getElementById("exampleInputpassword1", input.val());
+localStorage.setItem("exampleInputpassword1", input.val());
+
+
+console.log()
+
+
