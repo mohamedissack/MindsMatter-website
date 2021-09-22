c96440cd388ff580bc65803439ceecf665c33ff1
jepkess <biivincent1997@gmail.com>
2021-09-22 12:05:31 +0300

    get the value and print in console

--- Account/js/script.js
+++ Account/js/script.js
@@ -1,13 +1,32 @@
-var input = document.getElementById("name");
-localStorage.setItem("name", input.val());
-var input = document.getElementById("exampleInputEmail1");
-// localStorage.setItem("exampleInputEmail1", input.val());
-var input = document.getElementById("exampleInputPassword1");
-localStorage.setItem("exampleInputPassword1", input.val());
-var input = document.getElementById("exampleInputpassword1", input.val());
-localStorage.setItem("exampleInputpassword1", input.val());
+// function myfunction() {
+//     var inputtedusername = document.getElementById("name").value;
+//     // localStorage.setItem("name", input.val());
+//     var inputuseremail= document.getElementById("exampleInputEmail1").value;
+//     // localStorage.setItem("exampleInputEmail1", input.val());
+//     var inputpassword = document.getElementById("password").value;
+//     // localStorage.setItem("exampleInputPassword1", input.val());
+//     var confirm = document.getElementById("confirmpassword").value;
+//     // localStorage.setItem("exampleInputpassword1", input.val());
+
+//     console.log(inputtedusername);
+//     console.log(inputuseremail);
+//     console.log(inputpassword);
+//     console.log(confirm);
 
 
-console.log()
+// }
 
 
+
+// var inputtedusername,
+// element = document.getElementById("name");
+// if (element != null) {
+//     inputtedusername=element.value;
+// }
+// else{
+//     inputtedusername=null;
+// }
+// console.log(inputtedusername);
+
+var inputtedname = document.getElementById("name");
+console.log(inputtedname);
