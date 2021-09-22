caeadc6faf796ffb72748218166a8f83e2084fda
jepkess <biivincent1997@gmail.com>
2021-09-22 15:16:30 +0300

    modify js file

--- Account/js/script.js
+++ Account/js/script.js
@@ -1,12 +1,12 @@
 function myfunction() {
     var inputtedusername = document.getElementById("name").value;
-    localStorage.setItem("name", inputtedusername);
-    var inputuseremail= document.getElementById("exampleInputEmail1").value;
-    localStorage.setItem("exampleInputEmail1", inputuseremail);
+    // localStorage.setItem("name", inputtedusername);
+    var inputuseremail = document.getElementById("exampleInputEmail1").value;
+    // localStorage.setItem("exampleInputEmail1", inputuseremail);
     var inputpassword = document.getElementById("password").value;
-    localStorage.setItem("exampleInputPassword1", inputpassword);
+    // localStorage.setItem("exampleInputPassword1", inputpassword);
     var confirm = document.getElementById("confirmpassword").value;
-    localStorage.setItem("exampleInputpassword1", confirm);
+    // localStorage.setItem("exampleInputpassword1", confirm);
 
     console.log(inputtedusername);
     console.log(inputuseremail);
@@ -14,7 +14,16 @@ function myfunction() {
     console.log(confirm);
 
 
+    let Person = {
+        inputtedusername,
+        inputuseremail,
+        inputpassword,
+        confirm
+    };
+    localStorage.setItem("person". JSON.Stringify(Person));
+
 }
+
 
 
 
@@ -30,3 +39,4 @@ function myfunction() {
 
 // var inputtedname = document.getElementById("name");
 // console.log(inputtedname);
+
